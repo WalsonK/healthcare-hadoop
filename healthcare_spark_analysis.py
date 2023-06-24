5d535abdd0a6eb3d3f14211ce1e058ddb805bee8
WalsonK <71435596+WalsonK@users.noreply.github.com>
2023-06-24 20:18:46 +0200

    Schema modification + get distinct values method added

--- healthcare_spark_analysis.py
+++ healthcare_spark_analysis.py
@@ -7,31 +7,33 @@ class Healthcare:
     def __init__(self):
         self.spark_session = SparkSession.builder.master("local").appName("healthcare").getOrCreate()
         self.path = "hdfs://localhost:9000/user/walson/healthcare/"
+        self.healthcare_data = None
 
     def get_data(self, filename):
-        schema_dictionary = {"train_data.csv": (StructType()
-                                                .add("case_id", "integer")
-                                                .add("Hospital_code", "integer")
-                                                .add("Hospital_type_code", "string")
-                                                .add("City_Code_Hospital", "integer")
-                                                .add("Hospital_region_code", "string")
-                                                .add("Available Extra Rooms in Hospital", "integer")
-                                                .add("Department", "string")
-                                                .add("Ward_Type", "string")
-                                                .add("Ward_Facility_Code", "string")
-                                                .add("Bed Grade", "float")
-                                                .add("patientid", "integer")
-                                                .add("City_Code_Patient", "float")
-                                                .add("Type of Admission", "string")
-                                                .add("Severity of Illness", "string")
-                                                .add("Visitors with Patient", "integer")
-                                                .add("Age", "string")
-                                                .add("Admission_Deposit", "float")
-                                                .add("Stay", "string")
-                                                )
-                             }
+        file_schema = (StructType()
+                       .add("case_id", "integer")
+                       .add("Hospital_code", "integer")
+                       .add("Hospital_type_code", "string")
+                       .add("City_Code_Hospital", "integer")
+                       .add("Hospital_region_code", "string")
+                       .add("Available Extra Rooms in Hospital", "integer")
+                       .add("Department", "string")
+                       .add("Ward_Type", "string")
+                       .add("Ward_Facility_Code", "string")
+                       .add("Bed Grade", "float")
+                       .add("patientid", "integer")
+                       .add("City_Code_Patient", "float")
+                       .add("Type of Admission", "string")
+                       .add("Severity of Illness", "string")
+                       .add("Visitors with Patient", "integer")
+                       .add("Age", "string")
+                       .add("Admission_Deposit", "float")
+                       .add("Stay", "string")
+                       )
 
-        healthcare_data = self.spark_session.read.csv(self.path + filename, schema=schema_dictionary.get(filename),
-                                                      header=True)
+        self.healthcare_data = self.spark_session.read.csv(self.path + filename, schema=file_schema, header=True)
 
-        healthcare_data.show(5)
+    def get_distinct_values(self, column_name):
+        if self.healthcare_data is None:
+            raise AttributeError("Healthcare_data undefined")
+        return self.healthcare_data.select(column_name).distinct().collect()
--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import healthcare_spark_analysis
 if __name__ == '__main__':
     healthcare_instance = healthcare_spark_analysis.Healthcare()
     healthcare_instance.get_data("train_data.csv")
+    print(healthcare_instance.get_distinct_values("Department"))
